51d06218f35772446f8ca8bfb4b6e68c09d71b9c
Candela Puerta <109047221+candelabp@users.noreply.github.com>
2025-07-23 23:20:44 -0300

    arregle tailwind

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,6 +1,15 @@
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-quicksand mb-4">Welcome to BeautyConnect</h1>
+      <p className="text-lg font-quicksand text-gray-700">Your one-stop solution for beauty services.</p>
+      <button className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+        Get Started
+      </button>
+      <h1 className="text-red-500 text-3xl font-bold">Funciona Tailwind</h1>
+
+    </div>
+
   )
 }
 
