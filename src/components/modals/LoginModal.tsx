86c28832934180ae5daeaacab83e0a0aac522595
Candela Puerta <109047221+candelabp@users.noreply.github.com>
2025-07-29 20:55:03 -0300

    blur

--- src/components/modals/LoginModal.tsx
+++ src/components/modals/LoginModal.tsx
@@ -34,7 +34,7 @@ const LoginModal = ({ isOpen, onClose }: LoginModalProps) => {
 };
 
   return (
-    <div className="fixed inset-0  bg-opacity-40 flex items-center justify-center z-50">
+    <div className="fixed inset-0   backdrop-blur bg-opacity-40 flex items-center justify-center z-50">
       <div className="bg-white p-6 rounded-lg w-full max-w-sm shadow-lg relative">
         <button className="absolute top-2 right-3 text-gray-500 hover:text-gray-800" onClick={handleClose}>
           ✕
