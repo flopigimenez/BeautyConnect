0e032d982994cd1c8b5d8525b88b9775197652f7
flopigimenez <fgimenezmorcos@gmail.com>
2025-08-13 20:37:15 -0300

    arreglo registro

--- src/types/cliente/ClienteDTO.ts
+++ src/types/cliente/ClienteDTO.ts
@@ -2,6 +2,7 @@ import type { UsuarioDTO } from "../usuario/UsuarioDTO";
 
 export interface ClienteDTO {
     nombre: string;
+    apellido: string;
     telefono: number;
     usuario: UsuarioDTO;
 }
--- src/types/prestadorDeServicio/PestadorServicioDTO.ts
+++ src/types/prestadorDeServicio/PestadorServicioDTO.ts
@@ -2,6 +2,7 @@ import type { UsuarioDTO } from "../usuario/UsuarioDTO";
 
 export interface PrestadorServicioDTO {
     nombre: string;
+    apellido: string;
     telefono: number;
     usuario: UsuarioDTO;
 }
