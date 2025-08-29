84774eab34a8356a02e53550359d0f36f671b7c4
flopigimenez <fgimenezmorcos@gmail.com>
2025-08-29 12:59:33 -0300

    Update CustomTable.tsx

--- src/components/CustomTable.tsx
+++ src/components/CustomTable.tsx
@@ -18,6 +18,10 @@ interface CustomTableProps<T> {
   filtros?: {
     onClick: () => void;
   };
+  busqueda?: {
+    onChange: (value: string) => void;
+    placeholder?: string;
+  };
 }
 
 export function CustomTable<T extends object>({
@@ -26,6 +30,7 @@ export function CustomTable<T extends object>({
   title,
   actionButton,
   filtros,
+  busqueda,
 }: CustomTableProps<T>) {
   return (
     <div className="p-4">
@@ -48,6 +53,14 @@ export function CustomTable<T extends object>({
                 <IoFilterCircleOutline size={35} />
               </button>
             </div>
+          )}
+          {busqueda && (
+            <input
+              type="text"
+              onChange={(e) => busqueda.onChange(e.target.value)}              
+              placeholder={busqueda.placeholder || "Buscar..."}
+              className="border border-gray-300 rounded-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#C19BA8]"
+            />
           )}
         </div>
       )}
